ac21021ef390d00e1d3a6745368e2ccdffe6e1b8
oezingle <oezingle@gmail.com>
2022-03-01 13:02:35 -0800

    Wrote javascript

--- index.js
+++ index.js
@@ -1,26 +1,133 @@
-var start = document.getElementById("start");
-var stop = document.getElementById("stop");
-var timer = document.getElementById("timer");
-const date = Date();
-var startTime;
-var currentTime;
 
-function start()
-{	
-	startTime = date.getTime();
-	currentTime = startTime;
-	timer.innerHTML = 5-(currentTime/1000-startTime/1000);
-	setInterval(run(), 1000);
-	console.log("ssss");
-}
+const MINUTES = 0
+const SECONDS = 10
 
-function stop()
-{
+const CYCLE_MS = (MINUTES * 60 + SECONDS) * 1000
+
+// Simple event that calls a function if the time needed has expired
+const TimeEvent = class {
+	/**
+	 * Construct a timeEvent
+	 *
+	 * @param {number} msLeft the amount of milliseconds at which the event should fire
+	 * @param {function} callback a function that handles the event firing
+	 */
+	constructor (msLeft, callback) {
+		this.msLeft = msLeft
+		this.callback = callback
+
+		this.hasFired = false
+	}
+
+	/** 
+	 * Fire the event if the time left is less than the event's time, and it hasn't already fired
+	 *
+	 * @param {number} timeLeft the ms left on the timer
+	 */
+	fire(timeLeft) {
+		if (timeLeft <= this.msLeft && !this.hasFired) {
+			this.callback(timeLeft)
+
+			this.hasFired = true
+		}
+	}
 	
+	/**
+	 * Reset the timeEvent for another round
+	 */
+	reset () {
+		this.hasFired = false
+	}
 }
 
-function run()
-{	
-	currentTime = date.getTime();
-	timer.innerHTML = 5-(currentTime/1000-startTime/1000);
+// Global list of TimeEvents
+const timeEvents = [
+	new TimeEvent(100, (timeLeft) => {
+		console.log(`Time left: ${timeLeft}`)
+	})
+]
+
+const main = () => {
+	let startDate
+
+	let isRunning = false
+
+	let triggerStart = false
+
+	const start = () => {
+		timeEvents.forEach(timeEvent => {
+			timeEvent.reset()
+		})
+
+		triggerStart = true
+
+		isRunning = true
+	}
+
+	const startBtn = document.querySelector('#start')
+	const stopBtn = document.querySelector('#stop')
+
+	const timerText = document.querySelector('#timer')
+
+	startBtn.addEventListener('click', () => {
+		start()
+	})
+
+	stopBtn.addEventListener('click', () => {
+		isRunning = false
+	})
+	
+	// Meat and potatoes function runs every 200ms,
+	// updates the timer and checks that it hasn't stopped
+	setInterval(() => {
+		// Normalise start time to 200ms
+		if (triggerStart) {
+			startDate = Date.now()
+
+			triggerStart = false
+		}
+
+		const curr = Date.now()	
+
+		// Get the time left in the cycle if running, or reset if stopped	
+		const timeLeft = () => {
+			if (isRunning) {
+				return startDate + CYCLE_MS - curr
+			} else {
+				return CYCLE_MS
+			}
+		}
+
+		// Check that the timer hasn't expired
+		if (timeLeft() <= 0) {
+			// Automatically restart
+			start()
+		}
+
+		const left = timeLeft()
+
+		// Is slow but works enough
+		timeEvents.forEach(timeEvent => {
+			timeEvent.fire(left)
+		})
+		
+		// Format as date to get helper functions
+		const dateLeft = new Date(left)
+
+		const mins = dateLeft.getMinutes()
+
+		// padStart changes 0 to 00, so 5 minutes shows as 5:00
+		const secs = String(dateLeft.getSeconds()).padStart(2, '0')
+		
+		const timerContent = `${mins}:${secs}`
+
+		timerText.textContent = timerContent
+	}, 200)
 }
+
+//main()
+
+// Very precise timing because why not
+setTimeout(() => {
+	main()
+}, new Date(Date.now()).getMilliseconds % 200)
