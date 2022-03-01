a43153aff858950760cdd8392e55499a1e210be4
Deacon Sowerby <61608431+Deacon8@users.noreply.github.com>
2022-03-01 09:20:35 -0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+var timer = document.getElementById("timer");
+const d = Date();
+var currentTime;
+
+function start()
+{	
+	currentTime = d.getTime();
+	timer.innerHTML = ;
+	
+	
+}
+
+function stop()
+{
+	
+}
